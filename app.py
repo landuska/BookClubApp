5c5e6091da5936922376b8f5015bd1bdad628a42
Landysh Khusaenova <landysh.kh@gmail.com>
2026-04-22 14:45:11 +0200

    feat: add templates and home page route in Flask

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, Response, url_for, flash
+from data_manage import DataManager
 from models import *
 
 app = Flask(__name__)
-
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:dlyaraboty_Python2026@localhost:5432/postgres"
 
@@ -11,7 +11,9 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
-
+@app.route('/', methods=['GET'])
+def index()-> str:
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
