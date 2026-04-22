95e8f98d33fbecae9c9bc65c1ac1f48dbb19ebb3
Landysh Khusaenova <landysh.kh@gmail.com>
2026-04-22 11:35:31 +0200

    feat: add create method for author model and get methods for user, book, author and community models

--- data_manage.py
+++ data_manage.py
@@ -1,8 +1,10 @@
-from models import User, Book, UserBooks, Author, Community, UserCommunities, db
+from models import User, Book, Author, Community, db
 from sqlalchemy.exc import IntegrityError
+from datetime import date
 
 
 class DataManager():
+
     def add_user(self, name: str, password: str) -> None:
         existing_user = db.session.query(User).filter_by(name=name).first()
         if existing_user:
@@ -15,7 +17,6 @@ class DataManager():
             db.session.rollback()
             raise
 
-
     def add_book(self, book: Book) -> None:
         try:
             db.session.add(book)
@@ -26,15 +27,44 @@ class DataManager():
             db.session.rollback()
             raise
 
+    def add_author(self, name: str, birth_date: date, death_date=None) -> None:
+        existing_author = db.session.query(Author).filter_by(author_name=name).first()
+        if existing_author:
+            raise ValueError(f"Author with name '{name}' already exists.")
+        try:
+            new_author = Author(author_name=name, birth_date=birth_date, death_date=death_date)
+            db.session.add(new_author)
+            db.session.commit()
+        except Exception:
+            db.session.rollback()
+            raise
+
     def create_community(self, name: str) -> None:
         existing_community = db.session.query(Community).filter_by(community_name=name).first()
         if existing_community:
             raise ValueError(f"Community with name '{name}' already exists.")
         try:
-            new_community = Community(name=name)
+            new_community = Community(community_name=name)
             db.session.add(new_community)
             db.session.commit()
         except Exception:
             db.session.rollback()
             raise
 
+    def get_users(self) -> list[User]:
+        return db.session.query(User).all()
+
+    def get_books_by_user(self, user_id: int):
+        user = db.session.get(User, user_id)
+        if user:
+            return user.list_of_reading_books
+        return []
+
+    def get_authors(self) -> list[Author]:
+        return db.session.query(Author).all()
+
+    def get_communities_by_user(self, user_id: int):
+        user = db.session.get(User, user_id)
+        if user:
+            return user.list_of_communities_of_user
+        return []
